07b1fe52a3ac7ae71446c762887f8f8b9aba866b
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-12-14 16:23:02 +0330

    def loop test

---
+++ codeforces/Team.py
@@ -0,0 +1,12 @@
+def first_solution(my_list):
+    for i in my_list:
+        print(i)
+
+"""num = int(input())
+my_list = []
+
+for i in range(0, num):
+    input_list = list(map(lambda x: int(x), input().split()))
+    my_list.append(input_list)"""
+
+first_solution([[1, 1, 0], [1, 0, 0], [0, 0, 0], [0, 0, 1]])
