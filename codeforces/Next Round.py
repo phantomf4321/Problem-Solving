604fb26b772b1388caf038fbd169452c16548693
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-12-14 19:55:28 +0330

    def

--- codeforces/Next Round.py
+++ codeforces/Next Round.py
@@ -1,6 +1,11 @@
 def first_solution(scores, rank):
     counter = 0
     scores.sort(reverse=True)
-    return scores
+    rank = scores[rank - 1]
+    for i in scores:
+        if(i >= rank):
+            counter += 1
+
+    return counter
 
 print(first_solution([5,7,9,7,7,10,8], 1))
