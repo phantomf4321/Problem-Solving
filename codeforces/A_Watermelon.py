4cc746cf6f2dedd8a6fae0ad9bee2ab599f790fc
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-12-14 16:10:19 +0330

    first_solution

---
+++ codeforces/A_Watermelon.py
@@ -0,0 +1,15 @@
+def first_solution(a):
+    b = 0
+    c = 0
+    confirm = 0
+    for i in range(1, a):
+        c = i
+        b = a - i
+        if (b % 2 == 0 and c % 2 == 0):
+            print("YES")
+            confirm = 1
+            break
+    if (confirm == 0):
+        print("NO")
+x = int(input())
+first_solution(x)
