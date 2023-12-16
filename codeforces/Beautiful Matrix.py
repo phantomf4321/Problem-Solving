d753af395c9a1e7bc72f20974e81dc2f3d784796
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-12-16 19:06:31 +0330

    test

--- codeforces/Beautiful Matrix.py
+++ codeforces/Beautiful Matrix.py
@@ -1,6 +1,8 @@
 def first_solution(m):
     d = dict( (j,(x, y)) for x, i in enumerate(m) for y, j in enumerate(i) )
-    return d[1]
+    index1 = abs(2 - d[1][0])
+    index2 = abs(2 - d[1][1])
+    return index1 + index2
 
 matrix = []
 for i in range(0, 5):
