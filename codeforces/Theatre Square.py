e771a9cee44083486744928529845fac5e992c48
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-12-16 19:20:54 +0330

    round

---
+++ codeforces/Theatre Square.py
@@ -0,0 +1,16 @@
+def round(a):
+    b = int(a)
+    if (a == b):
+        return a
+    else:
+        return a+1
+
+def first_solution(m, n, a):
+    return 0
+
+a = 6
+b = 4
+c = a/b
+print(c)
+c = int(c)
+print(c)
