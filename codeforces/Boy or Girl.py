386777e21e96027cea66e4baaa497b326b9a060c
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2024-03-03 21:22:02 +0330

    test def

--- codeforces/Boy or Girl.py
+++ codeforces/Boy or Girl.py
@@ -1,9 +1,9 @@
-my_string = "asdfds"
-unique_words = set([*my_string])
-print(len(unique_words))
 def first_solution(my_string):
     unique_words = set([*my_string])
     if(len(unique_words)%2 == 0):
         return("CHAT WITH HER!")
     else:
         return("IGNORE HIM!")
+
+my_string = "xiaodao"
+print(first_solution(my_string))
