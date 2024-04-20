20c872c27abfe0753b3360b234212ca7ddc2365b
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2024-04-20 18:36:18 +0330

    Update Stones on the Table.py

--- codeforces/Stones on the Table.py
+++ codeforces/Stones on the Table.py
@@ -1,1 +1,11 @@
-
+def solve(n, s):
+    resault = 0
+    for i in range(n-1):
+        if s[i] == s[i+1]:
+            resault = resault+1
+            
+    return resault
+    
+n = int(input())
+s = input()
+print(solve(n, s))
