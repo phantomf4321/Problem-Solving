7304fcfa66cf7ee7d427976ff9b7697253385037
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2024-04-23 15:43:14 +0330

    Update Soldier and Bananas.py

--- codeforces/Soldier and Bananas.py
+++ codeforces/Soldier and Bananas.py
@@ -1,1 +1,15 @@
-
+def banana(k, n, w):
+    counter = 1
+    saver = 0
+    while counter <= w:
+        saver = saver + counter*k
+        counter = counter + 1
+    
+    res = saver - n
+    if res < 0:
+        return 0
+    else:
+        return res
+    
+k, n, w = map(int, input().split())
+print(banana(k, n, w))
